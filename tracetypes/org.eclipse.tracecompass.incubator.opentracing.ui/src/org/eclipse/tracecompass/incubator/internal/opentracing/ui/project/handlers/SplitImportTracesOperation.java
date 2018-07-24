b518093632689673b4c77f3ce8fe7c2d33f05e77
Katherine Nadeau <katherine.nadeau@ericsson.com>
2018-07-24 14:51:25 -0400

    opentracing: SplitTracesHandler for multiple traces in a same file
    Jaeger .json tracing files can contain more than one trace. This feature
    allows you to right click on the trace in the project explorer and
    "Split traces" into multiple traces.
    
    Change-Id: I18f1be2cedd9279a002cf9172e207b715fe64274
    Signed-off-by: Katherine Nadeau <katherine.nadeau@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/126626
    Tested-by: CI Bot
    Reviewed-by: Simon Delisle <simon.delisle@ericsson.com>
    Tested-by: Simon Delisle <simon.delisle@ericsson.com>
    Reviewed-by: Jean-Christian Kouame <jean-christian.kouame@ericsson.com>

---
+++ tracetypes/org.eclipse.tracecompass.incubator.opentracing.ui/src/org/eclipse/tracecompass/incubator/internal/opentracing/ui/project/handlers/SplitImportTracesOperation.java
@@ -0,0 +1,109 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.incubator.internal.opentracing.ui.project.handlers;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.tracecompass.tmf.core.project.model.TmfTraceType;
+import org.eclipse.tracecompass.tmf.core.project.model.TraceTypeHelper;
+import org.eclipse.tracecompass.tmf.ui.project.model.TmfTraceFolder;
+import org.eclipse.tracecompass.tmf.ui.project.model.TmfTraceTypeUIUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+/**
+ * Operator to split jaeger traces into multiple files and import them
+ *
+ * @author Katherine Nadeau
+ *
+ */
+public class SplitImportTracesOperation {
+
+    private static final String TRACE_TYPE_ID = "org.eclipse.tracecompass.incubator.opentracing.core"; //$NON-NLS-1$
+
+    private static final String DATA_KEY = "data"; //$NON-NLS-1$
+    private static final String TRACE_ID = "traceID"; //$NON-NLS-1$
+    private static final String TOTAL_KEY = "total"; //$NON-NLS-1$
+    private static final String LIMIT_KEY = "limit"; //$NON-NLS-1$
+    private static final String OFFSET_KEY = "offset"; //$NON-NLS-1$
+    private static final String ERRORS_KEY = "errors"; //$NON-NLS-1$
+
+    /**
+     * Takes a JsonObject of multiple jaeger traces and import the selected traces
+     * it contains
+     *
+     * @param jsonObject
+     *            Complete json object of the traces file
+     * @param selectedTracesId
+     *            List of trace id's to import or null to import all
+     * @param newTracesDestination
+     *            Path of the new traces folder relative to the destination folder
+     * @param tracesDestinationFolder
+     *            "Traces" folder of the trace to split
+     */
+    public static void splitAndImport(JsonObject jsonObject, List<String> selectedTracesId, String newTracesDestination, TmfTraceFolder tracesDestinationFolder) {
+        TraceTypeHelper traceTypeHelper = TmfTraceType.getTraceType(TRACE_TYPE_ID);
+
+        Gson gson = new Gson();
+        JsonArray array = jsonObject.get(DATA_KEY).getAsJsonArray();
+        JsonElement total = jsonObject.get(TOTAL_KEY);
+        JsonElement limit = jsonObject.get(LIMIT_KEY);
+        JsonElement offset = jsonObject.get(OFFSET_KEY);
+        JsonElement errors = jsonObject.get(ERRORS_KEY);
+
+        String newFolderPath = tracesDestinationFolder.getLocation().getPath() + newTracesDestination;
+        new File(newFolderPath).mkdir();
+
+        // We create a new file for every selected trace in the traces file
+        for (int i = 0; i < array.size(); i++) {
+            String traceId = array.get(i).getAsJsonObject().get(TRACE_ID).getAsString();
+
+            if (selectedTracesId == null || selectedTracesId.contains(traceId)) {
+                String fileName = traceId + ".json"; //$NON-NLS-1$
+                File subTrace = new File(newFolderPath + '/' + fileName);
+
+                try {
+                    subTrace.createNewFile();
+
+                    JsonObject newObject = new JsonObject();
+                    JsonArray data = new JsonArray();
+                    data.add(array.get(i));
+                    newObject.add(DATA_KEY, data);
+                    newObject.add(TOTAL_KEY, total);
+                    newObject.add(LIMIT_KEY, limit);
+                    newObject.add(OFFSET_KEY, offset);
+                    newObject.add(ERRORS_KEY, errors);
+                    try (PrintWriter tempWriter = new PrintWriter(subTrace)) {
+                        tempWriter.print(gson.toJson(newObject));
+                    }
+
+                    IFolder resource = tracesDestinationFolder.getResource();
+                    resource.refreshLocal(IResource.DEPTH_INFINITE, null);
+
+                    IFile file = resource.getFile(newTracesDestination + '/' + fileName);
+
+                    TmfTraceTypeUIUtils.setTraceType(file, traceTypeHelper);
+                } catch (IOException | CoreException e) {
+                }
+            }
+        }
+    }
+
+}
---
+++ tracetypes/org.eclipse.tracecompass.incubator.opentracing.ui/src/org/eclipse/tracecompass/incubator/internal/opentracing/ui/project/handlers/SplitTracesHandler.java
@@ -0,0 +1,179 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.incubator.internal.opentracing.ui.project.handlers;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.tracecompass.tmf.ui.project.model.TmfCommonProjectElement;
+import org.eclipse.tracecompass.tmf.ui.project.model.TmfTraceElement;
+import org.eclipse.tracecompass.tmf.ui.project.model.TmfTraceFolder;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
+
+/**
+ * Handler used to separate multiple open tracing traces in a same .json file
+ *
+ * @author Katherine Nadeau
+ *
+ */
+public class SplitTracesHandler extends AbstractHandler {
+
+    private enum ImportConflictOptions {
+        RENAME, OVERWRITE, SKIP;
+    }
+
+    private static final String TRACE_TYPE_ID = "org.eclipse.tracecompass.incubator.opentracing.core"; //$NON-NLS-1$
+
+    private static final String DATA_KEY = "data"; //$NON-NLS-1$
+
+    @Override
+    public boolean isEnabled() {
+        // Get the selection
+        IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+        IWorkbenchPart part = page.getActivePart();
+        if (part == null) {
+            return false;
+        }
+        ISelectionProvider selectionProvider = part.getSite().getSelectionProvider();
+        if (selectionProvider == null) {
+            return false;
+        }
+        ISelection selection = selectionProvider.getSelection();
+        Object firstElement = ((IStructuredSelection) selection).getFirstElement();
+        final TmfCommonProjectElement traceElem = (firstElement instanceof TmfTraceElement) ? ((TmfTraceElement) firstElement).getElementUnderTraceFolder() : (TmfCommonProjectElement) firstElement;
+
+        if (!traceElem.getTraceType().equals(TRACE_TYPE_ID)) {
+            return false;
+        }
+
+        int arraySize = 0;
+
+        try (FileReader fileReader = new FileReader(traceElem.getLocation().getPath())) {
+            try (JsonReader reader = new JsonReader(fileReader)) {
+                Gson gson = new Gson();
+                JsonObject object = gson.fromJson(reader, JsonObject.class);
+                arraySize = object.get(DATA_KEY).getAsJsonArray().size();
+            }
+        } catch (IOException e) {
+            return false;
+        }
+
+        return arraySize > 1;
+    }
+
+    @Override
+    public @Nullable Object execute(@Nullable ExecutionEvent event) throws ExecutionException {
+        ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
+        Object firstElement = ((IStructuredSelection) selection).getFirstElement();
+        final TmfCommonProjectElement traceElem = (firstElement instanceof TmfTraceElement) ? ((TmfTraceElement) firstElement).getElementUnderTraceFolder() : (TmfCommonProjectElement) firstElement;
+
+        TmfTraceFolder destinationFolder = traceElem.getProject().getTracesFolder();
+        if (destinationFolder == null) {
+            return null;
+        }
+
+        // Path for the new folder under Traces for the subTraces
+        IPath traceElemPath = traceElem.getPath().removeFileExtension();
+        String newTracesDestination = traceElemPath.makeRelativeTo(destinationFolder.getPath()).toString();
+        if (ResourcesPlugin.getWorkspace().getRoot().findMember(traceElemPath) != null) {
+            String index = startImportConflictDialog(traceElemPath);
+            if (index == null) {
+                return null;
+            }
+            newTracesDestination += index;
+        }
+
+        // We read, split and import the new traces
+        try (FileReader fileReader = new FileReader(traceElem.getLocation().getPath())) {
+            try (JsonReader reader = new JsonReader(fileReader)) {
+                Gson gson = new Gson();
+                JsonObject object = gson.fromJson(reader, JsonObject.class);
+                SplitImportTracesOperation.splitAndImport(object, null, newTracesDestination, destinationFolder);
+            }
+        } catch (IOException e) {
+        }
+
+        return null;
+    }
+
+    private static String startImportConflictDialog(IPath tracePath) {
+        IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+        Shell shell = window.getShell();
+        final MessageDialog dialog = new MessageDialog(shell,
+                "Confirmation", //$NON-NLS-1$
+                null,
+                NLS.bind("Folder with name {0} already exists in project. Do you want to rename, overwrite or skip?", tracePath.lastSegment()), //$NON-NLS-1$
+                MessageDialog.QUESTION, new String[] {
+                        "Rename", //$NON-NLS-1$
+                        "Overwrite", //$NON-NLS-1$
+                        "Skip" //$NON-NLS-1$
+                }, 2) {
+            @Override
+            protected int getShellStyle() {
+                return super.getShellStyle() | SWT.SHEET;
+            }
+        };
+
+        final int[] returnValue = new int[1];
+        shell.getDisplay().syncExec(new Runnable() {
+
+            @Override
+            public void run() {
+                returnValue[0] = dialog.open();
+            }
+
+        });
+
+        switch (ImportConflictOptions.values()[returnValue[0]]) {
+        case RENAME:
+            return nextIndex(tracePath);
+        case OVERWRITE:
+            return ""; //$NON-NLS-1$
+        case SKIP:
+        default:
+            return null;
+        }
+    }
+
+    private static String nextIndex(IPath tracePath) {
+        IResource existingResource = ResourcesPlugin.getWorkspace().getRoot().findMember(tracePath);
+        IContainer folder = existingResource.getParent();
+
+        for (int i = 2; true; i++) {
+            String index = '(' + Integer.toString(i) + ')';
+            if (folder.findMember(existingResource.getName() + index) == null) {
+                return index;
+            }
+        }
+    }
+}
