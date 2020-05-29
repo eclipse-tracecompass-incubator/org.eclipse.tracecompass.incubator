d20d93d18c0f58dbb879cda2e409b81be0e501b8
Geneviève Bastien <gbastien+lttng@versatic.net>
2020-05-29 13:05:58 -0400

    multiView: Get the legend of tree viewer by ID
    Instead of using the name to get query the legend, the ID of the entry
    is used and should thus match the XY chart's legend.
    
    Change-Id: I64279aa46b1895536dda0aac628b1b5f0ebbaeb7
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>
    Reviewed-on: https://git.eclipse.org/r/163857
    Tested-by: Trace Compass Bot <tracecompass-bot@eclipse.org>
    Reviewed-by: Ivan Grinenko <ivan.grinenko@auriga.com>

--- analyses/org.eclipse.tracecompass.incubator.tmf.ui.multiview.ui/src/org/eclipse/tracecompass/incubator/internal/tmf/ui/multiview/ui/view/xychart/TreeViewer.java
+++ analyses/org.eclipse.tracecompass.incubator.tmf.ui.multiview.ui/src/org/eclipse/tracecompass/incubator/internal/tmf/ui/multiview/ui/view/xychart/TreeViewer.java
@@ -77,7 +77,10 @@ public class TreeViewer extends AbstractSelectTreeViewer {
                     && isChecked(element)) {
                 Optional<ITmfTreeDataModel> model = tryGetModel(element);
                 if (model.isPresent()) {
-                    return getLegendImage(model.get().getName());
+                    ITmfTreeDataModel treeModel = model.get();
+                    if (treeModel.hasRowModel()) {
+                        return getLegendImage(treeModel.getId());
+                    }
                 }
             }
             return null;
