4412431682f844047a249765d7a9e61ef748ee03
Bernd Hufmann <Bernd.Hufmann@ericsson.com>
2021-01-19 15:53:03 -0500

    server: add error handling for errors during trace server startup
    - Add --launcher.suppressErrors to the server launch configuration. This
    will make sure that non-zero exit values don't opened an error dialog
    and the error is printed on the console instead
    - Add -nosplash to the the server launch configuration. Even if there is
    no splash screen defined for the server, this flag explicitly hides
    any splash screen
    - Return non-zero exit code for various startup errors and log error
    to the Eclipse log under ${workspace-folder}/.metadata/.log.
    
    Change-Id: I06534cc7f683f5938a91e17adfc18866e9bc7fc6
    Signed-off-by: Bernd Hufmann <Bernd.Hufmann@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass.incubator/org.eclipse.tracecompass.incubator/+/175072
    Tested-by: Patrick Tasse <patrick.tasse@gmail.com>
    Tested-by: Trace Compass Bot <tracecompass-bot@eclipse.org>
    Reviewed-by: Patrick Tasse <patrick.tasse@gmail.com>

--- trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core/src/org/eclipse/tracecompass/incubator/internal/trace/server/jersey/rest/core/application/Application.java
+++ trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core/src/org/eclipse/tracecompass/incubator/internal/trace/server/jersey/rest/core/application/Application.java
@@ -13,11 +13,16 @@ package org.eclipse.tracecompass.incubator.internal.trace.server.jersey.rest.cor
 import java.io.File;
 import java.net.URL;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.osgi.service.datalocation.Location;
 import org.eclipse.tracecompass.incubator.internal.trace.server.jersey.rest.core.webapp.WebApplication;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
 /**
  * Eclipse application entry
@@ -27,22 +32,44 @@ import org.eclipse.tracecompass.incubator.internal.trace.server.jersey.rest.core
 public class Application implements IApplication {
 
     private static final String WORKSPACE_NAME = ".tracecompass-webapp"; //$NON-NLS-1$
+
+    private enum ExitCode {
+        OK(""), //$NON-NLS-1$
+        ERROR_NO_WORKSPACE_ROOT("Trace server error: Workspace root doesn't exist."), //$NON-NLS-1$
+        ERROR_WORKSPACE_ROOT_PERMISSION("Trace server error: Workspace not writable."), //$NON-NLS-1$
+        ERROR_WORKSPACE_ALREADY_IN_USE("Trace server error: Workspace already in use."), //$NON-NLS-1$
+        ERROR_RUNNING_TRACE_SERVER("Trace server error: Error running trace server."); //$NON-NLS-1$
+        @NonNull private String fErrorText;
+        private ExitCode(@NonNull String errorText) {
+            fErrorText = errorText;
+        }
+        @NonNull public String getErrorText() {
+            return fErrorText;
+        }
+    }
+
     private WebApplication fWebAppl;
     private Location fInstanceLoc = null;
+
 
     @Override
     public Object start(IApplicationContext context) throws Exception {
         // fetch the Location that we will be modifying
         fInstanceLoc = Platform.getInstanceLocation();
+        Bundle bundle = FrameworkUtil.getBundle(this.getClass());
         if (!fInstanceLoc.allowsDefault() && !fInstanceLoc.isSet()) {
             File workspaceRoot = new File(getWorkspaceRoot());
 
             if (!workspaceRoot.exists()) {
-                return IApplication.EXIT_OK;
+                ExitCode exitCode = ExitCode.ERROR_NO_WORKSPACE_ROOT;
+                Platform.getLog(bundle).log(new Status(IStatus.ERROR, bundle.getSymbolicName(), exitCode.getErrorText()));
+                return Integer.valueOf(exitCode.ordinal());
             }
 
             if (!workspaceRoot.canWrite()) {
-                return IApplication.EXIT_OK;
+                ExitCode exitCode = ExitCode.ERROR_WORKSPACE_ROOT_PERMISSION;
+                Platform.getLog(bundle).log(new Status(IStatus.ERROR, bundle.getSymbolicName(), exitCode.getErrorText()));
+                return Integer.valueOf(exitCode.ordinal());
             }
 
             String workspace = getWorkspaceRoot() + File.separator + WORKSPACE_NAME;
@@ -51,12 +78,20 @@ public class Application implements IApplication {
         }
 
         if (!fInstanceLoc.lock()) {
-            return IApplication.EXIT_OK;
+            ExitCode exitCode = ExitCode.ERROR_WORKSPACE_ALREADY_IN_USE;
+            Platform.getLog(bundle).log(new Status(IStatus.ERROR, bundle.getSymbolicName(), exitCode.getErrorText() + " (workspace: " + fInstanceLoc.getURL().getPath() + ")"));//$NON-NLS-1$ //$NON-NLS-2$
+            return Integer.valueOf(exitCode.ordinal());
         }
 
         fWebAppl = new WebApplication();
-        fWebAppl.start();
-        return null;
+        try {
+            fWebAppl.start();
+        } catch (Exception e) {
+            ExitCode exitCode = ExitCode.ERROR_RUNNING_TRACE_SERVER;
+            Platform.getLog(bundle).log(new Status(IStatus.ERROR, bundle.getSymbolicName(), exitCode.getErrorText(), e));
+            return Integer.valueOf(exitCode.ordinal());
+        }
+        return IApplication.EXIT_OK;
     }
 
     @Override
