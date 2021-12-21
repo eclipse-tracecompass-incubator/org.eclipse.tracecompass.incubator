9d3e764522e90fe9b1d3cc0182338314868733d9
Marco Miller <marco.miller@ericsson.com>
2021-12-21 10:59:58 -0500

    server: Add swagger-core documentation for /health
    Add documentation to HealthService's endpoint, based on [1] below unless
    adjusting text. Do not include [1]'s manually documented "DOWN" status
    though, as the latter cannot be possibly returned by an unavailable
    server anyway. Keep the 503 response case for documentation purposes.
    
    Still, duly document the currently only possible (running) server status
    "UP". Add the consistent IServerStatus model interface and enum for that
    matter, despite this status being the (currently) sole one supported.
    
    Reuse this introduced "UP" enum value (name), thus remove the no longer
    used UP_VALUE in HealthService. Fix the previous, likely copy-paste typo
    in getHealthStatus's @return javadoc tag while being there. Remove the
    unnecessary curly braces in @Produces while reformatting in Eclipse.
    
    [1] https://github.com/theia-ide/trace-server-protocol/blob/master/API.yaml
    
    Change-Id: Ibdc7fc73c435cac44a9b1e00e6d89c77063996ec
    Signed-off-by: Marco Miller <marco.miller@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass.incubator/org.eclipse.tracecompass.incubator/+/189058
    Tested-by: Trace Compass Bot <tracecompass-bot@eclipse.org>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core/src/org/eclipse/tracecompass/incubator/internal/trace/server/jersey/rest/core/services/HealthService.java
+++ trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core/src/org/eclipse/tracecompass/incubator/internal/trace/server/jersey/rest/core/services/HealthService.java
@@ -17,8 +17,14 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.tracecompass.incubator.internal.trace.server.jersey.rest.core.model.IServerStatus;
+
 import com.google.common.collect.ImmutableMap;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
@@ -31,18 +37,20 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public class HealthService {
 
     private static final String STATUS_KEY = "status"; //$NON-NLS-1$
-    private static final String UP_VALUE = "UP"; //$NON-NLS-1$
 
     /**
      * Getter for the health status
      *
-     * @return The set of opened experiments
+     * @return UP status.
      */
     @GET
-    @Produces({ MediaType.APPLICATION_JSON })
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Get the health status of this server", responses = {
+            @ApiResponse(responseCode = "200", description = "The trace server is running and ready to receive requests", content = @Content(schema = @Schema(implementation = IServerStatus.class))),
+            @ApiResponse(responseCode = "503", description = "The trace server is unavailable or in maintenance and cannot receive requests")
+    })
     public Response getHealthStatus() {
         // If the server can answer this call, it is up!!
-        return Response.ok(ImmutableMap.of(STATUS_KEY, UP_VALUE)).build();
+        return Response.ok(ImmutableMap.of(STATUS_KEY, IServerStatus.Status.UP.name())).build();
     }
-
 }
