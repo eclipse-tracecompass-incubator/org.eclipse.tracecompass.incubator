0ac288b92722f442608894812e46ec94a569c86f
Hoang Thuan Pham <hoang.pham@calian.ca>
2022-02-24 10:57:16 -0500

    Improve file offset tracker for FTrace byte buffer
    This commit improves the offset tracker of the BinaryFTraceByteBuffer
    class. Currently, the BinaryFTraceByteBuffer utilizes a ByteBuffer to
    read data from the trace file. The ByteBuffer instance reads data in
    chunks of a predefined amount of bytes. The existing implementation of
    the offset tracker returns the offset of the ByteBuffer instance.
    However, the expected returned value of the offset tracker is the offset
    of the next byte in the ByteBuffer. This commit changes the behavior of
    the offset tracker so that it behaves as expected.
    
    In addition, it adds a unit test to make sure that the offset tracker
    functions correctly.
    
    Change-Id: Ic6b45fb293a7c077b0211632d4740a191630aad3
    Signed-off-by: Hoang Thuan Pham <hoang.pham@calian.ca>
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass.incubator/org.eclipse.tracecompass.incubator/+/191195
    Tested-by: Trace Compass Bot <tracecompass-bot@eclipse.org>
    Tested-by: Marco Miller <marco.miller@ericsson.com>
    Reviewed-by: Marco Miller <marco.miller@ericsson.com>
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass.incubator/org.eclipse.tracecompass.incubator/+/191545
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

---
+++ tracetypes/org.eclipse.tracecompass.incubator.ftrace.core/src/org/eclipse/tracecompass/incubator/internal/ftrace/core/binary/parser/BinaryFTraceHeaderElementSize.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0 which
+ * accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.incubator.internal.ftrace.core.binary.parser;
+
+/**
+ * This file contains the number of bytes reserved for different sections in the
+ * header of a binary ftrace file.
+ *
+ * @author Hoang Thuan Pham
+ */
+public class BinaryFTraceHeaderElementSize {
+
+    private BinaryFTraceHeaderElementSize() {
+        // Do nothing
+    }
+
+    /**
+     * Each string in the header is terminated by /0. The size of the
+     * termination character is 1 byte
+     */
+    public static final int STRING_TERMINATOR = 1;
+}
