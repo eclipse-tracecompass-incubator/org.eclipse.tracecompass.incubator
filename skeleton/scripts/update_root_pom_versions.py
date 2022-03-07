0153218c9edf67a8cfc4686967895b358cdf9a41
Bernd Hufmann <Bernd.Hufmann@ericsson.com>
2022-03-07 15:04:54 -0500

    releng: update versions to 0.1.0 and add scripts to update versions
    This will help in the future to update version numbers for features
    and RCP. Going forward, for each mainline Trace Compass release, the
    version numbers will be increased, to be able to easier track changes.
    
    Change-Id: I843fd049efb4b95fbbf8eb9c9f7138e548a3beff
    Signed-off-by: Bernd Hufmann <Bernd.Hufmann@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass.incubator/org.eclipse.tracecompass.incubator/+/191575
    Tested-by: Trace Compass Bot <tracecompass-bot@eclipse.org>
    Tested-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-by: Patrick Tasse <patrick.tasse@gmail.com>

---
+++ skeleton/scripts/update_root_pom_versions.py
@@ -0,0 +1,33 @@
+###############################################################################
+# Copyright (c) 2016, 2022 Ericsson
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
+# which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+###############################################################################
+
+#args: filename oldversion newversion
+
+#Update root pom.xml versions. For example
+#   <artifactId>org.eclipse.tracecompass</artifactId>
+#-  <version>2.0.0-SNAPSHOT</version>
+#+  <version>2.0.1-SNAPSHOT</version>
+#
+# Also the target plarform:
+#
+#               <classifier>${target-platform}</classifier>
+#-              <version>2.0.0-SNAPSHOT</version>
+#+              <version>2.0.1-SNAPSHOT</version>
+#             </artifact>
+
+import sys, re
+if len(sys.argv) < 4:
+    sys.exit('Usage: python update_root_pom_versions.py [file] [old version] [new version]')
+fileContent = open(sys.argv[1]).read()
+fileContent = re.sub("(<artifactId>org.eclipse.tracecompass.*</artifactId>\n\s+)<version>" + sys.argv[2] + "-SNAPSHOT</version>", "\g<1><version>" + sys.argv[3] + "-SNAPSHOT</version>", fileContent)
+# Also the target platform version being used
+fileContent = re.sub("<version>" + sys.argv[2] + "-SNAPSHOT</version>(\n\s+</artifact>)", "<version>" + sys.argv[3] + "-SNAPSHOT</version>\g<1>", fileContent)
+open(sys.argv[1], "w").write(fileContent)
---
+++ skeleton/scripts/update_version.sh
@@ -0,0 +1,55 @@
+###############################################################################
+# Copyright (c) 2016, 2022 Ericsson
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
+# which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+###############################################################################
+
+# Updates the version numbers of the project
+#
+# Which versions are updated? At the time of writing, plug-ins with
+# code are updated when the API changes (API additions or breakage)
+# by using the API tooling inside Eclipse. So this script does not
+# deal with those.
+# Things that follow the project version number (subject to change):
+# - Features
+# - Documentation plugins
+# - Product related stuff (about text, branding plugins, etc)
+# - Parent pom version
+
+# Usage  ./update_version.sh oldversion newversion
+# For example ./update_version.sh 2.1.0 2.2.0
+#
+# Note that can also make the version go backwards if needed.
+
+oldVersion=$1
+newVersion=$2
+
+if [ -z "$oldVersion" -o -z "$newVersion" ]; then
+	echo "usage: $0 oldversion newversion"
+	exit 1
+fi
+
+echo Changing $oldVersion to $newVersion
+
+#Update root pom version
+find ../.. -maxdepth 1 -name "pom.xml" -exec python update_root_pom_versions.py {} $oldVersion $newVersion \;
+#Update pom.xml with <parent> tag with the new version of the root pom, excluding the poms in the skeleton directory
+find ../.. -name skeleton -prune -o -name "pom.xml" -type f -exec python update_parent_pom_versions.py {} $oldVersion $newVersion \;
+#Update doc plugin versions
+find ../../doc -name "MANIFEST.MF" -exec sed -i -e s/$oldVersion.qualifier/$newVersion.qualifier/g {} \;
+
+#Update feature versions (feature.xml)
+find ../.. -name "feature.xml" -exec sed -i -e s/$oldVersion.qualifier/$newVersion.qualifier/g {} \;
+
+#Update branding plugin manifest.MF
+sed -i -e s/$oldVersion.qualifier/$newVersion.qualifier/g ../../rcp/org.eclipse.tracecompass.incubator.rcp.branding/META-INF/MANIFEST.MF
+#rcp/org.eclipse.tracecompass.incubator.rcp.branding/plugin.xml aboutText
+sed -i -e s/$oldVersion/$newVersion/g ../../rcp/org.eclipse.tracecompass.incubator.rcp.branding/plugin.xml
+
+#Update .product rcp/org.eclipse.tracecompass.incubator.rcp.product/tracing.incubator.product
+sed -i -e s/$oldVersion/$newVersion/g ../../rcp/org.eclipse.tracecompass.incubator.rcp.product/tracing.incubator.product
