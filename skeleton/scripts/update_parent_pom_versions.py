cb25bbcc35c6cd1f8d8acb432dd10f6b12a5fd32
Bernd Hufmann <Bernd.Hufmann@ericsson.com>
2022-03-07 15:04:54 -0500

    releng: update versions to 0.1.0 and add scripts to update versions
    This will help in the future to update version numbers for features
    and RCP. Going forward, for each mainline Trace Compass release, the
    version numbers will be increased, to be able to easier track changes.
    
    Change-Id: I843fd049efb4b95fbbf8eb9c9f7138e548a3beff
    Signed-off-by: Bernd Hufmann <Bernd.Hufmann@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass.incubator/org.eclipse.tracecompass.incubator/+/191575
    Tested-by: Trace Compass Bot <tracecompass-bot@eclipse.org>
    Tested-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass.incubator/org.eclipse.tracecompass.incubator/+/191610

---
+++ skeleton/scripts/update_parent_pom_versions.py
@@ -0,0 +1,27 @@
+###############################################################################
+# Copyright (c) 2016, 2022 Ericsson
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
+# which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+###############################################################################
+
+#args: filename oldversion newversion
+
+#Update pom.xml version. For example
+#   <parent>
+#     <artifactId>org.eclipse.tracecompass</artifactId>
+#     <groupId>org.eclipse.tracecompass</groupId>
+#-    <version>2.0.0-SNAPSHOT</version>
+#+    <version>2.0.1-SNAPSHOT</version>
+#   </parent>
+
+import sys, re
+if len(sys.argv) < 4:
+    sys.exit('Usage: python update_parent_pom_versions.py [file] [old version] [new version]')
+fileContent = open(sys.argv[1]).read()
+fileContent = re.sub("<version>" + sys.argv[2] + "-SNAPSHOT</version>(\n\s+</parent>)", "<version>" + sys.argv[3] + "-SNAPSHOT</version>\g<1>", fileContent)
+open(sys.argv[1], "w").write(fileContent)
