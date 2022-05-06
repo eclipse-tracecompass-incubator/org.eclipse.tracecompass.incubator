fc169ab9ef3a001bdadaf9550fbd4cb9450e2de6
Rodrigo Pinto <rodrigo.pinto@calian.ca>
2022-05-06 10:25:38 -0400

    server: Add serializer for metadata in time graph entry model
    Also, add metadata and swagger annotation to TimeGraphEntry model.
    
    [Added] metadata to the serializer of time graph entry model.
    
    Change-Id: I0ebc0641ca62ac1cbdfd08808078593b53812cf2
    Signed-off-by: Rodrigo Pinto <rodrigo.pinto@calian.ca>
    Signed-off-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass.incubator/org.eclipse.tracecompass.incubator/+/193246
    Tested-by: Trace Compass Bot <tracecompass-bot@eclipse.org>
    Tested-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-by: Marco Miller <marco.miller@ericsson.com>
    Reviewed-by: Patrick Tasse <patrick.tasse@gmail.com>

--- trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core/src/org/eclipse/tracecompass/incubator/internal/trace/server/jersey/rest/core/model/TimeGraphEntry.java
+++ trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core/src/org/eclipse/tracecompass/incubator/internal/trace/server/jersey/rest/core/model/TimeGraphEntry.java
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2021 Ericsson
+ * Copyright (c) 2021, 2022 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License 2.0 which
@@ -10,6 +10,9 @@
  **********************************************************************/
 
 package org.eclipse.tracecompass.incubator.internal.trace.server.jersey.rest.core.model;
+
+import java.util.Collection;
+import java.util.Map;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -30,4 +33,11 @@ public interface TimeGraphEntry {
      */
     @Schema(description = "End of the range for which this entry exists")
     long getEnd();
+
+    /**
+     * @return The entry's metadata map.
+     */
+    @Schema(description = "Optional metadata map for domain specific data for matching data across data providers. Keys for the same data shall be the same across data providers. "
+            + "Only values of type Number or String are allowed. For each key all values shall have the same type.")
+    Map<String, Collection<Object>> getMetadata();
 }
