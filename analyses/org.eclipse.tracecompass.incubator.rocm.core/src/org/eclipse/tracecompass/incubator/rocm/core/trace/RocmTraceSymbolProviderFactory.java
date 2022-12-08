c8fc56209182f1bdba20a8ec6dc7f1beb8399193
Bernd Hufmann <bernd.hufmann@ericsson.com>
2022-12-08 14:14:12 -0500

    Bug 581189: Fix Flame Chart symbol resolution for LTTng UST traces
    For that update the RocmTraceSymbolProviderFactory to not return a
    symbol provider if the relevant ROCM analysis is not applicable for a
    given trace. Also, it makes sure that the RocmTraceSymbolProvider
    doesn't return a symbol if the analysis is not applicable for a given
    trace (in the failing case the LTTng UST Trace).
    
    [Fixed] Flame Chart symbol resolution for LTTng UST traces
    
    Change-Id: I35adb2732df28e69947017856fd94022c05b95c6
    Signed-off-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass.incubator/org.eclipse.tracecompass.incubator/+/197573
    Tested-by: Trace Compass Bot <tracecompass-bot@eclipse.org>
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-by: Arnaud Fiorini <fiorini.arnaud@gmail.com>
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    (cherry picked from commit 71281103db7ccf073552cd3992129091919df443)
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass.incubator/org.eclipse.tracecompass.incubator/+/197508

--- analyses/org.eclipse.tracecompass.incubator.rocm.core/src/org/eclipse/tracecompass/incubator/rocm/core/trace/RocmTraceSymbolProviderFactory.java
+++ analyses/org.eclipse.tracecompass.incubator.rocm.core/src/org/eclipse/tracecompass/incubator/rocm/core/trace/RocmTraceSymbolProviderFactory.java
@@ -13,9 +13,11 @@ package org.eclipse.tracecompass.incubator.rocm.core.trace;
 
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.tracecompass.incubator.internal.rocm.core.analysis.RocmMetadataAnalysis;
 import org.eclipse.tracecompass.tmf.core.symbols.ISymbolProvider;
 import org.eclipse.tracecompass.tmf.core.symbols.ISymbolProviderFactory;
 import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
+import org.eclipse.tracecompass.tmf.core.trace.TmfTraceUtils;
 
 /**
  * Factory for the ROCm Symbol provider
@@ -26,6 +28,11 @@ public class RocmTraceSymbolProviderFactory implements ISymbolProviderFactory {
 
     @Override
     public @Nullable ISymbolProvider createProvider(@NonNull ITmfTrace trace) {
+        RocmMetadataAnalysis module = TmfTraceUtils.getAnalysisModuleOfClass(trace,
+                RocmMetadataAnalysis.class, RocmMetadataAnalysis.ID);
+        if (module == null) {
+            return null;
+        }
         return new RocmTraceSymbolProvider(trace);
     }
 }
