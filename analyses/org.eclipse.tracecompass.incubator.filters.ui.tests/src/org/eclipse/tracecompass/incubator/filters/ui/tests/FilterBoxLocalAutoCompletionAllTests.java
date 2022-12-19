02525763c3878c7441a92cb1ef5df8aac046f336
Marco Miller <marco.miller@ericsson.com>
2022-12-19 15:20:13 -0500

    filters.ui.tests: Fix FBLACompletionAllTests format
    Apply the default Eclipse formatting to this file which missed it prior.
    
    Change-Id: I0585a85b037bdbcc8a016eb94a1f5e1c3c5bf2fb
    Signed-off-by: Marco Miller <marco.miller@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass.incubator/org.eclipse.tracecompass.incubator/+/197756
    Tested-by: Trace Compass Bot <tracecompass-bot@eclipse.org>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- analyses/org.eclipse.tracecompass.incubator.filters.ui.tests/src/org/eclipse/tracecompass/incubator/filters/ui/tests/FilterBoxLocalAutoCompletionAllTests.java
+++ analyses/org.eclipse.tracecompass.incubator.filters.ui.tests/src/org/eclipse/tracecompass/incubator/filters/ui/tests/FilterBoxLocalAutoCompletionAllTests.java
@@ -11,18 +11,15 @@
 
 package org.eclipse.tracecompass.incubator.filters.ui.tests;
 
-
 import static org.junit.Assert.assertEquals;
 
 import org.eclipse.tracecompass.incubator.internal.filters.ui.lspFilterTextbox.FilterBoxLocalTextCompletion;
 import org.junit.Test;
 
 /**
- *
- *This tests the {@link FilterBoxLocalTextCompletion}
+ * This tests the {@link FilterBoxLocalTextCompletion}
  *
  * @author Maxime Thibault
- *
  */
 @SuppressWarnings("restriction")
 public class FilterBoxLocalAutoCompletionAllTests {
@@ -30,75 +27,71 @@ public class FilterBoxLocalAutoCompletionAllTests {
     /**
      * Tests quote completion
      */
-   @Test
-   public void completeQuoteTest() {
+    @Test
+    public void completeQuoteTest() {
 
-       //Double quote
-       assertEquals("\"\"", FilterBoxLocalTextCompletion.autocomplete("\"", 1));
+        // Double quote
+        assertEquals("\"\"", FilterBoxLocalTextCompletion.autocomplete("\"", 1));
 
-       assertEquals("t\"\"", FilterBoxLocalTextCompletion.autocomplete("t\"", 2));
+        assertEquals("t\"\"", FilterBoxLocalTextCompletion.autocomplete("t\"", 2));
 
-       assertEquals("\"t", FilterBoxLocalTextCompletion.autocomplete("\"t", 1));
+        assertEquals("\"t", FilterBoxLocalTextCompletion.autocomplete("\"t", 1));
 
-       assertEquals("\"\" ", FilterBoxLocalTextCompletion.autocomplete("\" ", 1));
+        assertEquals("\"\" ", FilterBoxLocalTextCompletion.autocomplete("\" ", 1));
 
+        // Single quote
+        assertEquals("''", FilterBoxLocalTextCompletion.autocomplete("'", 1));
 
-       //Single quote
-       assertEquals("''", FilterBoxLocalTextCompletion.autocomplete("'", 1));
+        assertEquals("t''", FilterBoxLocalTextCompletion.autocomplete("t'", 2));
 
-       assertEquals("t''", FilterBoxLocalTextCompletion.autocomplete("t'", 2));
+        assertEquals("'t", FilterBoxLocalTextCompletion.autocomplete("'t", 1));
 
-       assertEquals("'t", FilterBoxLocalTextCompletion.autocomplete("'t", 1));
+        assertEquals("'' ", FilterBoxLocalTextCompletion.autocomplete("' ", 1));
+    }
 
-       assertEquals("'' ", FilterBoxLocalTextCompletion.autocomplete("' ", 1));
+    /**
+     * Test brackets completions
+     */
+    @Test
+    public void completeBracket() {
 
-   }
+        // Squared brackets
+        assertEquals("[]", FilterBoxLocalTextCompletion.autocomplete("[", 1));
 
-   /**
-    * Test brackets completions
-    */
-   @Test
-   public void completeBracket() {
+        assertEquals("t[]", FilterBoxLocalTextCompletion.autocomplete("t[", 2));
 
-       //Squared brackets
-       assertEquals("[]", FilterBoxLocalTextCompletion.autocomplete("[", 1));
+        assertEquals("[t", FilterBoxLocalTextCompletion.autocomplete("[t", 1));
 
-       assertEquals("t[]", FilterBoxLocalTextCompletion.autocomplete("t[", 2));
+        assertEquals("[] ", FilterBoxLocalTextCompletion.autocomplete("[ ", 1));
 
-       assertEquals("[t", FilterBoxLocalTextCompletion.autocomplete("[t", 1));
+        assertEquals("[[]]", FilterBoxLocalTextCompletion.autocomplete("[[]", 2));
 
-       assertEquals("[] ", FilterBoxLocalTextCompletion.autocomplete("[ ", 1));
+        // Normal brackets
+        assertEquals("{}", FilterBoxLocalTextCompletion.autocomplete("{", 1));
 
-       assertEquals("[[]]", FilterBoxLocalTextCompletion.autocomplete("[[]", 2));
+        assertEquals("t{}", FilterBoxLocalTextCompletion.autocomplete("t{", 2));
 
-       //Normal brackets
-       assertEquals("{}", FilterBoxLocalTextCompletion.autocomplete("{", 1));
+        assertEquals("{t", FilterBoxLocalTextCompletion.autocomplete("{t", 1));
 
-       assertEquals("t{}", FilterBoxLocalTextCompletion.autocomplete("t{", 2));
+        assertEquals("{} ", FilterBoxLocalTextCompletion.autocomplete("{ ", 1));
 
-       assertEquals("{t", FilterBoxLocalTextCompletion.autocomplete("{t", 1));
+        assertEquals("{{}}", FilterBoxLocalTextCompletion.autocomplete("{{}", 2));
+    }
 
-       assertEquals("{} ", FilterBoxLocalTextCompletion.autocomplete("{ ", 1));
+    /**
+     * Tests parenthesis completion
+     */
+    @Test
+    public void completeParenthesis() {
 
-       assertEquals("{{}}", FilterBoxLocalTextCompletion.autocomplete("{{}", 2));
+        assertEquals("{}", FilterBoxLocalTextCompletion.autocomplete("{", 1));
 
-   }
+        assertEquals("t{}", FilterBoxLocalTextCompletion.autocomplete("t{", 2));
 
-   /**
-    * Tests parenthesis completion
-    */
-   @Test
-   public void completeParenthesis() {
+        assertEquals("{t", FilterBoxLocalTextCompletion.autocomplete("{t", 1));
 
-       assertEquals("{}", FilterBoxLocalTextCompletion.autocomplete("{", 1));
+        assertEquals("{} ", FilterBoxLocalTextCompletion.autocomplete("{ ", 1));
 
-       assertEquals("t{}", FilterBoxLocalTextCompletion.autocomplete("t{", 2));
-
-       assertEquals("{t", FilterBoxLocalTextCompletion.autocomplete("{t", 1));
-
-       assertEquals("{} ", FilterBoxLocalTextCompletion.autocomplete("{ ", 1));
-
-       assertEquals("{{}}", FilterBoxLocalTextCompletion.autocomplete("{{}", 2));
-
-}
+        assertEquals("{{}}", FilterBoxLocalTextCompletion.autocomplete("{{}", 2));
+    }
 }
