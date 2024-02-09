e8dccee3d689ea4073654d4907cd9a486deb4fe2
Fariba Daneshgar <faraji.daneshgar@gmail.com>
2024-02-09 11:12:56 -0500

    execution comparison:initial commit
    Differential flame graph view is updated to
    be able to show and hide Query part by demand.
    
    grouping are added to hamburger menu
    
    Dependencies to incubator for callstack and weighted tree packages are reslved
    
    changing in time ranges or in query reflect in graphical treeview and density charts
    
    Adding the reset Button to select entire trace (back to initial state)
    
    TODO
    
    * Add progress bar
    * instrument code(pretty good)
    * add test
    * make a registry for traceTYpe->callstack ID
    
    Signed-off-by: fariba <faraji.daneshgar@gmail.com>

--- analyses/org.eclipse.tracecompass.incubator.executioncomparision.core/src/org/eclipse/tracecompass/incubator/internal/executioncomparision/core/DifferentialCallGraphProvider.java
+++ analyses/org.eclipse.tracecompass.incubator.executioncomparision.core/src/org/eclipse/tracecompass/incubator/internal/executioncomparision/core/DifferentialCallGraphProvider.java
@@ -13,19 +13,20 @@ package org.eclipse.tracecompass.incubator.internal.executioncomparision.core;
 
 import java.util.Collection;
 
-import org.eclipse.tracecompass.analysis.profiling.core.base.ICallStackElement;
-import org.eclipse.tracecompass.analysis.profiling.core.base.ICallStackSymbol;
-import org.eclipse.tracecompass.analysis.profiling.core.base.IDataPalette;
 import org.eclipse.tracecompass.analysis.profiling.core.callgraph.AggregatedCallSite;
 import org.eclipse.tracecompass.analysis.profiling.core.callgraph.ICallGraphProvider2;
+import org.eclipse.tracecompass.analysis.profiling.core.base.ICallStackSymbol;
+import org.eclipse.tracecompass.analysis.profiling.core.base.IDataPalette;
 import org.eclipse.tracecompass.analysis.profiling.core.tree.IWeightedTreeProvider;
 import org.eclipse.tracecompass.analysis.profiling.core.tree.WeightedTree;
+import org.eclipse.tracecompass.analysis.profiling.core.base.ICallStackElement;
 import org.eclipse.tracecompass.incubator.analysis.core.weighted.tree.diff.DifferentialWeightedTree;
 import org.eclipse.tracecompass.incubator.analysis.core.weighted.tree.diff.DifferentialWeightedTreeProvider;
 import org.eclipse.tracecompass.incubator.analysis.core.weighted.tree.diff.DifferentialWeightedTreeSet;
 
 /**
- *
+ * DifferentialCallGraphProvider which provide a callgraphProvider. Inherited from DifferentialWeightedTreeProvider
+ * to edit naming and color ICallGraphProviderpalette to provide a differential flame graph
  * @author Fateme Faraji Daneshgar
  */
 public class DifferentialCallGraphProvider extends DifferentialWeightedTreeProvider<ICallStackSymbol> {
