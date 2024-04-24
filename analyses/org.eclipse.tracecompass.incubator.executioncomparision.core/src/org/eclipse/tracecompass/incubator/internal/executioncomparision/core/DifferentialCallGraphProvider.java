37239cc4ebed07586b8d03a74c02429ee77d9f7a
fariba <faraji.daneshgar@gmail.com>
2024-04-24 14:38:51 -0400

    execution comparison: adding reset button and some modifications
    Introduce a module to compare two groups of executions using a
    differential flame graph.
    
    An "EventDensityView" is used to select a time range for each group.
    Select the desired traces in an experiment.
    
    Showing the difference ratio based on self time (it  required a modification to
    AggregatedCalledFunction).
    
    Introducing the "ParametricWeightedTreeUtils" to get "SelfTime" instead of "duration"
    while building the differential Trees.
    
    Isolating duplicating flame graph
    
    TODO:
    
    * add grouping strategies (WALL TIME) Not Valid: It was
      Discussed for Async Trace Comparison
    * move grouping to hamburger menu
    * add progress bar
    * instrument code (pretty good)
    * add tests
    * Make work with mainline (should be a simple ID swap)
    * Make a registry for tracetype->callstack ID
    * remove hard disk operations from UI thread (in progress)
    * Add way to select the entire trace easily:
    * Add way to manually select precise time range (import from main?)
    * query will be added
    
    Signed-off-by: Fariba Daneshgar <faraji.daneshgar@gmail.com>
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

---
+++ analyses/org.eclipse.tracecompass.incubator.executioncomparision.core/src/org/eclipse/tracecompass/incubator/internal/executioncomparision/core/DifferentialCallGraphProvider.java
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * Copyright (c) 2023 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License 2.0 which
+ * accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.incubator.internal.executioncomparision.core;
+
+import java.util.Collection;
+
+import org.eclipse.tracecompass.analysis.profiling.core.base.ICallStackElement;
+import org.eclipse.tracecompass.analysis.profiling.core.base.ICallStackSymbol;
+import org.eclipse.tracecompass.analysis.profiling.core.base.IDataPalette;
+import org.eclipse.tracecompass.analysis.profiling.core.callgraph.AggregatedCallSite;
+import org.eclipse.tracecompass.analysis.profiling.core.callgraph.ICallGraphProvider2;
+import org.eclipse.tracecompass.analysis.profiling.core.tree.IWeightedTreeProvider;
+import org.eclipse.tracecompass.analysis.profiling.core.tree.WeightedTree;
+import org.eclipse.tracecompass.incubator.analysis.core.weighted.tree.diff.DifferentialWeightedTree;
+import org.eclipse.tracecompass.incubator.analysis.core.weighted.tree.diff.DifferentialWeightedTreeProvider;
+import org.eclipse.tracecompass.incubator.analysis.core.weighted.tree.diff.DifferentialWeightedTreeSet;
+
+/**
+ *
+ * @author Fateme Faraji Daneshgar
+ */
+public class DifferentialCallGraphProvider extends DifferentialWeightedTreeProvider<ICallStackSymbol> {
+
+    private final ICallGraphProvider2 fOriginalTree;
+
+    /**
+     * Constructor
+     *
+     * @param instrumentedCallStackAnalysis
+     *            the original tree
+     * @param trees
+     *            the other trees to compare
+     */
+    public DifferentialCallGraphProvider(IWeightedTreeProvider<ICallStackSymbol, ICallStackElement, AggregatedCallSite> instrumentedCallStackAnalysis,
+            Collection<DifferentialWeightedTree<ICallStackSymbol>> trees) {
+        this(instrumentedCallStackAnalysis, DifferentialWeightedTreeSet.<ICallStackSymbol> create(trees));
+    }
+
+    /**
+     * Constructor
+     *
+     * @param originalTree
+     *            The original tree provider, used to get information for texts
+     *            and metrics.
+     * @param treeSet
+     *            The differential tree set
+     */
+    @SuppressWarnings("unchecked")
+    public DifferentialCallGraphProvider(
+            IWeightedTreeProvider<ICallStackSymbol, ICallStackElement, ? extends WeightedTree<ICallStackSymbol>> originalTree,
+            DifferentialWeightedTreeSet<ICallStackSymbol> treeSet) {
+        super((IWeightedTreeProvider<ICallStackSymbol, ICallStackElement, WeightedTree<ICallStackSymbol>>)originalTree, treeSet);
+        fOriginalTree = (ICallGraphProvider2) originalTree;
+    }
+
+    @Override
+    public IDataPalette getPalette() {
+        return DifferentialFlamePalette.getInstance();
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    public String toDisplayString(DifferentialWeightedTree<ICallStackSymbol> tree) {
+        double difference = tree.getDifference();
+        WeightedTree<ICallStackSymbol> originalTree = tree.getOriginalTree();
+        String label = ""; //$NON-NLS-1$
+        if (originalTree instanceof AggregatedCallSite) {
+            label = fOriginalTree.toDisplayString((AggregatedCallSite) originalTree);
+        }
+        else {
+            label = String.valueOf(originalTree.getObject().resolve(null));
+        }
+        if (Double.isFinite(difference)) {
+            return String.format("(%#.02f %% ) %s", difference * 100, label); //$NON-NLS-1$
+        }
+        return label;
+
+    }
+
+}
