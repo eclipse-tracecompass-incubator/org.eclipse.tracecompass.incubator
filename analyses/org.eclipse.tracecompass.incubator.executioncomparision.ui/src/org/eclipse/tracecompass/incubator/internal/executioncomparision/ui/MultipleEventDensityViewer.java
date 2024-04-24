dce9e9aca137d5007f2d9ef2c9fe2024889127dc
fariba <faraji.daneshgar@gmail.com>
2024-04-24 14:38:51 -0400

    execution comparison: adding reset button and some modifications
    Introduce a module to compare two groups of executions using a
    differential flame graph.
    
    An "EventDensityView" is used to select a time range for each group.
    Select the desired traces in an experiment.
    
    Showing the difference ratio based on self time (it  required a modification to
    AggregatedCalledFunction).
    
    Introducing the "ParametricWeightedTreeUtils" to get "SelfTime" instead of "duration"
    while building the differential Trees.
    
    Isolating duplicating flame graph
    
    TODO:
    
    * add grouping strategies (WALL TIME) Not Valid: It was
      Discussed for Async Trace Comparison
    * move grouping to hamburger menu
    * add progress bar
    * instrument code (pretty good)
    * add tests
    * Make work with mainline (should be a simple ID swap)
    * Make a registry for tracetype->callstack ID
    * remove hard disk operations from UI thread (in progress)
    * Add way to select the entire trace easily:
    * Add way to manually select precise time range (import from main?)
    * query will be added
    
    Signed-off-by: Fariba Daneshgar <faraji.daneshgar@gmail.com>
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

---
+++ analyses/org.eclipse.tracecompass.incubator.executioncomparision.ui/src/org/eclipse/tracecompass/incubator/internal/executioncomparision/ui/MultipleEventDensityViewer.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2023 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License 2.0 which
+ * accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.incubator.internal.executioncomparision.ui;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.tracecompass.incubator.internal.executioncomparision.core.TmfCheckboxChangedSignal;
+import org.eclipse.tracecompass.internal.tmf.ui.viewers.eventdensity.EventDensityViewer;
+import org.eclipse.tracecompass.tmf.core.model.tree.TmfTreeDataModel;
+import org.eclipse.tracecompass.tmf.core.signal.TmfSelectionRangeUpdatedSignal;
+import org.eclipse.tracecompass.tmf.core.signal.TmfSignalHandler;
+import org.eclipse.tracecompass.tmf.core.signal.TmfSignalManager;
+import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
+import org.eclipse.tracecompass.tmf.core.trace.TmfTraceContext;
+import org.eclipse.tracecompass.tmf.core.trace.TmfTraceManager;
+import org.eclipse.tracecompass.tmf.ui.viewers.tree.ITmfTreeViewerEntry;
+import org.eclipse.tracecompass.tmf.ui.viewers.tree.TmfGenericTreeEntry;
+import org.eclipse.tracecompass.tmf.ui.viewers.xychart.linechart.TmfXYChartSettings;
+
+/**
+ *
+ */
+public class MultipleEventDensityViewer extends EventDensityViewer {
+
+    /**
+     * @param parent
+     *              Composite
+     * @param settings
+     *               ChartSetting
+     */
+    public MultipleEventDensityViewer(Composite parent, TmfXYChartSettings settings) {
+        super(parent, settings);
+    }
+
+    @Override
+    @TmfSignalHandler
+    public void selectionRangeUpdated(TmfSelectionRangeUpdatedSignal signal) {
+        if (signal == null) {
+            return;
+        }
+        final ITmfTrace trace = getTrace();
+        if (trace != null) {
+            TmfTraceContext ctx = TmfTraceManager.getInstance().getTraceContext(trace);
+            long selectedTime = ctx.getSelectionRange().getStartTime().toNanos();
+            long selectedEndTime = ctx.getSelectionRange().getEndTime().toNanos();
+            setSelectionRange(selectedTime, selectedEndTime);
+        }
+
+    }
+
+    @Override
+    public void handleCheckStateChangedEvent(Collection<ITmfTreeViewerEntry> entries) {
+        super.handleCheckStateChangedEvent(entries);
+            List<String> traceNames = new ArrayList<>();
+            for (ITmfTreeViewerEntry entry : entries) {
+                if (entry instanceof TmfGenericTreeEntry) {
+                    TmfGenericTreeEntry<TmfTreeDataModel> genericEntry = (TmfGenericTreeEntry<TmfTreeDataModel>) entry;
+                    String name = genericEntry.getModel().getName();
+                    traceNames.add(name);
+                }
+            }
+            TmfSignalManager.dispatchSignal(new TmfCheckboxChangedSignal(this, traceNames));
+        }
+
+
+
+}
