3e78ba8421f3c7afc614434c4e27f2d59877453d
Greg Savin <gsavin@mips.com>
2024-05-16 09:48:23 -0700

    Added shinro tracetype source artifacts (skeletal so far)

---
+++ tracetypes/org.eclipse.tracecompass.incubator.shinro.tracetype.core/src/org/eclipse/tracecompass/incubator/shinro/tracetype/core/ShinroTrace.java
@@ -0,0 +1,12 @@
+package org.eclipse.tracecompass.incubator.shinro.tracetype.core;
+
+import org.eclipse.tracecompass.lttng2.ust.core.trace.LttngUstTrace;
+import org.eclipse.tracecompass.tmf.ctf.core.trace.CtfTmfTrace;
+
+public class ShinroTrace extends LttngUstTrace {
+
+}
+
+class Foo extends CtfTmfTrace {
+
+}
