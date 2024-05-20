f3e37a5606807186b55f966578d97b3f4219c6b0
Greg Savin <gsavin@mips.com>
2024-05-20 14:23:46 -0700

    This diff gets Counters view to show up (albeit with a strikethrough). Still need to override some base class method(s) in order for the strikethrough to go away.

---
+++ tracetypes/org.eclipse.tracecompass.incubator.shinro.tracetype.core/src/org/eclipse/tracecompass/incubator/internal/shinro/tracetype/core/counters/ShinroCountersAnalysis.java
@@ -0,0 +1,7 @@
+package org.eclipse.tracecompass.incubator.internal.shinro.tracetype.core.counters;
+
+import org.eclipse.tracecompass.analysis.counters.core.CounterAnalysis;
+
+public class ShinroCountersAnalysis extends CounterAnalysis {
+
+}
