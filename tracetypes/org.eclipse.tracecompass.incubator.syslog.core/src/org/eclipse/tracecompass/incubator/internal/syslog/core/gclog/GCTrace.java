0ea45669381335554fb127cf23c0b4cfd702c629
Matthew Khouzam <matthew.khouzam@ericsson.com>
2024-06-11 00:17:24 -0400

    syslog: gclog add columns to events
    Change-Id: Ib85992d60e88f7d83c7266d13bd91972a9a6df23
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- tracetypes/org.eclipse.tracecompass.incubator.syslog.core/src/org/eclipse/tracecompass/incubator/internal/syslog/core/gclog/GCTrace.java
+++ tracetypes/org.eclipse.tracecompass.incubator.syslog.core/src/org/eclipse/tracecompass/incubator/internal/syslog/core/gclog/GCTrace.java
@@ -5,13 +5,16 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.tracecompass.incubator.internal.syslog.core.gclog.event.GCEvent;
+import org.eclipse.tracecompass.incubator.internal.syslog.core.gclog.event.eventInfo.GCMemoryItem;
 import org.eclipse.tracecompass.incubator.internal.syslog.core.gclog.model.GCModel;
 import org.eclipse.tracecompass.incubator.internal.syslog.core.gclog.parser.GCLogParser;
 import org.eclipse.tracecompass.incubator.internal.syslog.core.gclog.parser.GCLogParserFactory;
@@ -20,6 +23,8 @@ import org.eclipse.tracecompass.tmf.core.event.ITmfEventField;
 import org.eclipse.tracecompass.tmf.core.event.TmfEvent;
 import org.eclipse.tracecompass.tmf.core.event.TmfEventField;
 import org.eclipse.tracecompass.tmf.core.event.TmfEventType;
+import org.eclipse.tracecompass.tmf.core.event.aspect.ITmfEventAspect;
+import org.eclipse.tracecompass.tmf.core.event.aspect.TmfEventFieldAspect;
 import org.eclipse.tracecompass.tmf.core.exceptions.TmfTraceException;
 import org.eclipse.tracecompass.tmf.core.timestamp.ITmfTimestamp;
 import org.eclipse.tracecompass.tmf.core.timestamp.TmfTimestamp;
@@ -108,17 +113,26 @@ public class GCTrace extends TmfTrace {
         ITmfTimestamp time = TmfTimestamp.fromNanos((long) (event.getStartTime() * 1e6));
         List<TmfEventField> fields = new ArrayList<>();
         fields.add(new TmfEventField("Allocation", event.getAllocation(), null)); //$NON-NLS-1$
-        fields.add(new TmfEventField("gc id", event.getGcid(), null)); //$NON-NLS-1$
-        fields.add(new TmfEventField("cause", event.getCause(), null)); //$NON-NLS-1$
-        fields.add(new TmfEventField("pause", event.getPause(), null)); //$NON-NLS-1$
-        fields.add(new TmfEventField("cause Interval", event.getCauseInterval(), null)); //$NON-NLS-1$
+        fields.add(new TmfEventField("GC Id", event.getGcid(), null)); //$NON-NLS-1$
+        fields.add(new TmfEventField("Cause", event.getCause(), null)); //$NON-NLS-1$
+        fields.add(new TmfEventField("Pause", event.getPause(), null)); //$NON-NLS-1$
+        fields.add(new TmfEventField("Cause Interval", event.getCauseInterval(), null)); //$NON-NLS-1$
         fields.add(new TmfEventField("Reclamation", event.getReclamation(), null)); //$NON-NLS-1$
         fields.add(new TmfEventField("Cpu time", event.getCpuTime(), null)); //$NON-NLS-1$
         fields.add(new TmfEventField("Duration", event.getDuration(), null)); //$NON-NLS-1$
         fields.add(new TmfEventField("Level", event.getEventLevel(), null)); //$NON-NLS-1$
         fields.add(new TmfEventField("Promotion", event.getPromotion(), null)); //$NON-NLS-1$
         fields.add(new TmfEventField("End time", event.getEndTime(), null)); //$NON-NLS-1$
-
+        GCMemoryItem[] memoryItems = event.getMemoryItems();
+        if (memoryItems != null) {
+            for (GCMemoryItem item : memoryItems) {
+                if (item != null) {
+                    fields.add(new TmfEventField("mem" + item.getArea().getName() + "-pre", item.getPreUsed(), null)); //$NON-NLS-1$ //$NON-NLS-2$
+                    fields.add(new TmfEventField("mem" + item.getArea().getName() + "-post", item.getPostUsed(), null)); //$NON-NLS-1$ //$NON-NLS-2$
+                    fields.add(new TmfEventField("mem" + item.getArea().getName() + "-capacity", item.getPostCapacity(), null)); //$NON-NLS-1$ //$NON-NLS-2$
+                }
+            }
+        }
         return new TmfEvent(this, -1, time, type, new TmfEventField(ITmfEventField.ROOT_FIELD_ID, null, fields.toArray(new TmfEventField[0])));
     }
 
@@ -128,6 +142,28 @@ public class GCTrace extends TmfTrace {
             return ((Number) locationInfo).intValue();
         }
         return 0;
+    }
+
+    @Override
+    public @NonNull Iterable<ITmfEventAspect<?>> getEventAspects() {
+        List<ITmfEventAspect<?>> aspects = new ArrayList<>();
+        aspects.addAll((Collection<? extends ITmfEventAspect<?>>) super.getEventAspects());
+        aspects.add(2, simpleAspect("Allocation")); //$NON-NLS-1$
+        aspects.add(3, simpleAspect("Reclamation")); //$NON-NLS-1$
+        aspects.add(3, simpleAspect("Promotion")); //$NON-NLS-1$
+        aspects.add(4, simpleAspect("GC Id")); //$NON-NLS-1$
+        aspects.add(5, simpleAspect("Cause")); //$NON-NLS-1$
+        aspects.add(6, simpleAspect("Cause Interval")); //$NON-NLS-1$
+        aspects.add(7, simpleAspect("Cpu time")); //$NON-NLS-1$
+        aspects.add(8, simpleAspect("Duration")); //$NON-NLS-1$
+        aspects.add(9, simpleAspect("Pause")); //$NON-NLS-1$
+        aspects.add(10, simpleAspect("Level")); //$NON-NLS-1$
+
+        return aspects;
+    }
+
+    private static TmfEventFieldAspect simpleAspect(String field) {
+        return new TmfEventFieldAspect(field, field, event -> event.getContent());
     }
 
 }
