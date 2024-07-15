581e03ac5b8bf70fc7de003e18b4e9ebbf0495e7
Bernd Hufmann <bernd.hufmann@ericsson.com>
2024-07-15 11:19:05 -0400

    server: Skeleton code for configuration service per data provider
    Includes also swagger doc updates.
    
    [Added] Skeleton code for configuration service per data provider
    
    Signed-off-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core/src/org/eclipse/tracecompass/incubator/internal/trace/server/jersey/rest/core/services/EndpointConstants.java
+++ trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core/src/org/eclipse/tracecompass/incubator/internal/trace/server/jersey/rest/core/services/EndpointConstants.java
@@ -84,6 +84,7 @@ public final class EndpointConstants {
     static final String DT = "Data Tree"; //$NON-NLS-1$
     static final String EXP = "Experiments"; //$NON-NLS-1$
     static final String IDF = "Identifier"; //$NON-NLS-1$
+    static final String OCG = "Output Configurations"; //$NON-NLS-1$
     static final String STY = "Styles"; //$NON-NLS-1$
     static final String TGR = "TimeGraph"; //$NON-NLS-1$
     static final String TRA = "Traces"; //$NON-NLS-1$
