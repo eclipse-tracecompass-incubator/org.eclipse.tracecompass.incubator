2f2f98926198a13402c99f7f0964d9ae266bfe06
Bernd Hufmann <bernd.hufmann@ericsson.com>
2024-08-28 09:09:14 -0400

    dpdk: Remove unused imports warnings dpdk.core plug-in
    Signed-off-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- analyses/org.eclipse.tracecompass.incubator.dpdk.core/src/org/eclipse/tracecompass/incubator/internal/dpdk/core/lcore/analysis/DpdkLogicalCoreAnalysisModule.java
+++ analyses/org.eclipse.tracecompass.incubator.dpdk.core/src/org/eclipse/tracecompass/incubator/internal/dpdk/core/lcore/analysis/DpdkLogicalCoreAnalysisModule.java
@@ -13,11 +13,7 @@ package org.eclipse.tracecompass.incubator.internal.dpdk.core.lcore.analysis;
 import static org.eclipse.tracecompass.common.core.NonNullUtils.checkNotNull;
 
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
-import org.eclipse.tracecompass.analysis.os.linux.core.tid.TidAnalysisModule;
-import org.eclipse.tracecompass.tmf.core.analysis.IAnalysisModule;
 import org.eclipse.tracecompass.tmf.core.analysis.requirements.TmfAbstractAnalysisRequirement;
 import org.eclipse.tracecompass.tmf.core.analysis.requirements.TmfAbstractAnalysisRequirement.PriorityLevel;
 import org.eclipse.tracecompass.tmf.core.analysis.requirements.TmfAnalysisEventRequirement;
@@ -25,7 +21,6 @@ import org.eclipse.tracecompass.tmf.core.statesystem.ITmfStateProvider;
 import org.eclipse.tracecompass.tmf.core.statesystem.TmfStateSystemAnalysisModule;
 import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
 import org.eclipse.tracecompass.tmf.core.trace.TmfTrace;
-import org.eclipse.tracecompass.tmf.core.trace.TmfTraceUtils;
 
 import com.google.common.collect.ImmutableList;
 
