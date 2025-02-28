97f4a5b24e0a751165f8aa96498a7c55bc2d0ae2
Elwazir, Ammar <Ammar.Elwazir@amd.com>
2025-02-28 09:32:07 -0600

    Adding perfetto support for TC

---
+++ analyses/org.eclipse.tracecompass.incubator.perfetto.core.tests/src/org/eclipse/tracecompass/perfetto/core/tests/PerfettoTest.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2025 AMD
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0 which
+ * accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.incubator.perfetto.core.tests;
+
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertTrue;
+//
+//import org.eclipse.jdt.annotation.NonNull;
+//import org.eclipse.tracecompass.tmf.core.exceptions.TmfTraceException;
+//import org.eclipse.tracecompass.tmf.core.timestamp.ITmfTimestamp;
+//import org.eclipse.tracecompass.tmf.core.timestamp.TmfTimestamp;
+//import org.eclipse.tracecompass.tmf.core.trace.ITmfContext;
+//import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
+//import org.junit.Test;
+
+/**
+ * Test generic Perfetto trace
+ *
+ * @author Ammar ELWazir
+ */
+public class PerfettoTest {
+
+}
