e21117c3ff13849a9b512c094ca18033251e949e
Adel Belkhiri <adel.belkhiri@gmail.com>
2025-03-07 13:29:51 -0500

    dpdk: fix lcore role and status inconsistency
    This commit fixes the following issues in the LCore analysis:
    
     1. The ROLE_SERVICE lcore role was never assigned.
     2. Certain lcore roles and statuses were not displayed because Enum
     values were not converted to strings before saving them to the state system.
    
    These fixes ensure proper role and status assignment alongside visibility in the UI.
    
    Signed-off-by: Adel Belkhiri <adel.belkhiri@gmail.com>

--- tracetypes/org.eclipse.tracecompass.incubator.dpdk.core/src/org/eclipse/tracecompass/incubator/internal/dpdk/core/lcore/analysis/LogicalCore.java
+++ tracetypes/org.eclipse.tracecompass.incubator.dpdk.core/src/org/eclipse/tracecompass/incubator/internal/dpdk/core/lcore/analysis/LogicalCore.java
@@ -10,6 +10,7 @@
  **********************************************************************/
 package org.eclipse.tracecompass.incubator.internal.dpdk.core.lcore.analysis;
 
+import java.util.Objects;
 import org.eclipse.tracecompass.incubator.internal.dpdk.core.Activator;
 import org.eclipse.tracecompass.statesystem.core.ITmfStateSystemBuilder;
 
@@ -23,85 +24,174 @@ import org.eclipse.tracecompass.statesystem.core.ITmfStateSystemBuilder;
 public class LogicalCore {
 
     /* Logical Core attribute names */
-    private static final String LCORES = "LCORES"; //$NON-NLS-1$
+    private static final String LCORES = "LCores"; //$NON-NLS-1$
     private static final String LCORE_FUNCTION = "LCORE_FUNCTION"; //$NON-NLS-1$
     private static final String LCORE_ROLE = "LCORE_ROLE"; //$NON-NLS-1$
     private static final String LCORE_STATUS = "LCORE_STATUS"; //$NON-NLS-1$
 
     /* Service attribute names */
-    private static final String SERVICES = "services"; //$NON-NLS-1$
+    private static final String SERVICES = "Services"; //$NON-NLS-1$
     private static final String SERVICE_CORE = "service_core"; //$NON-NLS-1$
     private static final String SERVICE_NAME = "service_name"; //$NON-NLS-1$
     private static final String SERVICE_STATUS = "service_status"; //$NON-NLS-1$
 
     /**
-     * Values for the different roles that a logical core can take
+     * Values for the different roles that a worker logical core can take
      */
-    public final class LogicalCoreRole {
+    enum LogicalCoreRole {
         /** The logical core is running in the runtime environment */
-        public static final int ROLE_RTE = 0;
+        ROLE_RTE(String.valueOf(Messages.LCoreRole_rte)),
         /** The logical core is not running in DPDK */
-        public static final int ROLE_OFF = 1;
+        ROLE_OFF(String.valueOf(Messages.LCoreRole_off)),
         /** The logical core is running the service */
-        public static final int ROLE_SERVICE = 2;
+        ROLE_SERVICE(String.valueOf(Messages.LCoreRole_service)),
         /** The logical core is not running in the EAL */
-        public static final int ROLE_NON_EAL = 3;
+        ROLE_NON_EAL(String.valueOf(Messages.LCoreRole_non_eal)),
+        /** The role of the logical core is unknown */
+        ROLE_UNKNOWN(String.valueOf(Messages.LCoreRole_unknown));
+
+        private final String fLabel;
+
+        /**
+         * Constructor
+         *
+         * @param label
+         *            String identifying the role of a LCore
+         */
+        LogicalCoreRole(String label) {
+            this.fLabel = label;
+        }
+
+        /**
+         * Get the label associated with this LCore role
+         *
+         * @return the label string
+         */
+        public String getLabel() {
+            return fLabel;
+        }
+
+        /**
+         * Convert to a LCore role from an integer
+         *
+         * @param value
+         *            integer encoding the ordinal order of the LCore role
+         * @return an {@link LogicalCoreRole}
+         */
+        public static LogicalCoreRole fromInt(int value) {
+            LogicalCoreRole[] roles = LogicalCoreRole.values();
+            if (value < 0 || value >= roles.length) {
+                return ROLE_UNKNOWN;
+            }
+            return Objects.requireNonNull(roles[value]);
+        }
     }
 
     enum LogicalCoreStatus {
-        /** Lcore is waiting for a new command */
-        IDLE,
-        /** Lcore is running */
-        RUNNING,
-        /** Not yet ready to start scheduling services */
-        DISABLED,
-        /** Command is executed */
-        OFF;
+        /** LCore is waiting for a new command */
+        IDLE(String.valueOf(Messages.LCoreStatus_idle)),
+        /** LCore is running */
+        RUNNING(String.valueOf(Messages.LCoreStatus_running)),
+        /** LCore status is unknown */
+        UNKNOWN(String.valueOf(Messages.LCoreStatus_unknown));
+
+        private final String fLabel;
+
+        /**
+         * Constructor
+         *
+         * @param label
+         *            String identifying the status of a LCore
+         */
+        LogicalCoreStatus(String label) {
+            this.fLabel = label;
+        }
+
+        /**
+         * Get the label associated with this LCore status
+         *
+         * @return the label string
+         */
+        public String getLabel() {
+            return fLabel;
+        }
+
     }
 
     enum ServiceStatus {
-        /** Unknown Status */
-        UNKNOWN,
         /** Service has been registered */
-        REGISTERED,
+        REGISTERED(String.valueOf(Messages.ServiceStatus_registered)),
         /** Service is Disabled */
-        DISABLED,
+        DISABLED(String.valueOf(Messages.ServiceStatus_disabled)),
         /** Service is Enabled */
-        ENABLED,
+        ENABLED(String.valueOf(Messages.ServiceStatus_enabled)),
         /** Service is enabled but waiting to be executed */
-        PENDING,
+        PENDING(String.valueOf(Messages.ServiceStatus_pending)),
         /** Service is running */
-        RUNNING;
+        RUNNING(String.valueOf(Messages.ServiceStatus_running)),
+        /** Service status is Unknown */
+        UNKNOWN(String.valueOf(Messages.ServiceStatus_unknown));
+
+        private final String fLabel;
+
+        /**
+         * Constructor
+         *
+         * @param label
+         *            String identifying the status of a service
+         */
+        ServiceStatus(String label) {
+            this.fLabel = label;
+        }
+
+        /**
+         * Get the label associated with this service status
+         *
+         * @return the label string
+         */
+        public String getLabel() {
+            return fLabel;
+        }
     }
 
     /**
+     * Assign a role and a status to a logical core
+     *
      * @param ssb
      *            State system builder
      * @param newRole
-     *            Role integer value to input in the state system
+     *            Role to input in the state system
      * @param lcoreId
      *            identifier number for the logical core
      * @param timestamp
      *            time to use for state change
      */
-    public static void setRole(ITmfStateSystemBuilder ssb, Integer newRole, Integer lcoreId, long timestamp) {
-        LogicalCoreStatus newStatus;
-        if (newRole == LogicalCoreRole.ROLE_RTE) {
-            newStatus = LogicalCoreStatus.IDLE;
-        } else if (newRole == LogicalCoreRole.ROLE_OFF) {
-            newStatus = LogicalCoreStatus.OFF;
-        } else {
-            Activator.getInstance().logWarning("LogicalCore setRole with unexpected role value " + newRole.toString()); //$NON-NLS-1$
-            newStatus = LogicalCoreStatus.IDLE;
+    public static void setRole(ITmfStateSystemBuilder ssb, LogicalCoreRole newRole, int lcoreId, long timestamp) {
+
+        // Change the status if the newRole is not service
+        if (newRole != LogicalCoreRole.ROLE_SERVICE) {
+            final LogicalCoreStatus newStatus;
+            if (newRole == LogicalCoreRole.ROLE_RTE) {
+                newStatus = LogicalCoreStatus.IDLE;
+            } else if (newRole == LogicalCoreRole.ROLE_OFF
+                    || newRole == LogicalCoreRole.ROLE_NON_EAL) {
+                newStatus = LogicalCoreStatus.UNKNOWN;
+            } else {
+                Activator.getInstance().logWarning("LogicalCore setRole with unexpected role value!"); //$NON-NLS-1$
+                newStatus = LogicalCoreStatus.UNKNOWN;
+            }
+            setStatus(ssb, newStatus, lcoreId, timestamp);
         }
-        setStatus(ssb, newStatus, lcoreId, timestamp);
-        // Update State system
-        int lcoreQuark = ssb.getQuarkAbsoluteAndAdd(LCORES, String.valueOf(lcoreId));
-        int lcoreRoleQuark = ssb.getQuarkRelativeAndAdd(lcoreQuark, LCORE_ROLE);
-        ssb.modifyAttribute(timestamp, newRole, lcoreRoleQuark);
+
+        // Update the state system
+        final int lcoreQuark = ssb.getQuarkAbsoluteAndAdd(LCORES, String.valueOf(lcoreId));
+        final int lcoreRoleQuark = ssb.getQuarkRelativeAndAdd(lcoreQuark, LCORE_ROLE);
+        ssb.modifyAttribute(timestamp, newRole.getLabel(), lcoreRoleQuark);
     }
 
     /**
+     * Set the status of a logical core
+     *
      * @param ssb
      *            State System builder
      * @param newStatus
@@ -114,10 +204,12 @@ public class LogicalCore {
     public static void setStatus(ITmfStateSystemBuilder ssb, LogicalCoreStatus newStatus, Integer lcoreId, long timestamp) {
         int lcoreQuark = ssb.getQuarkAbsoluteAndAdd(LCORES, String.valueOf(lcoreId));
         int lcoreRoleQuark = ssb.getQuarkRelativeAndAdd(lcoreQuark, LCORE_STATUS);
-        ssb.modifyAttribute(timestamp, newStatus, lcoreRoleQuark);
+        ssb.modifyAttribute(timestamp, newStatus.getLabel(), lcoreRoleQuark);
     }
 
     /**
+     * Record the function pointer executed by a logical core
+     *
      * @param ssb
      *            State System builder
      * @param function
@@ -179,6 +271,6 @@ public class LogicalCore {
      */
     public static void setServiceStatus(ITmfStateSystemBuilder ssb, ServiceStatus serviceStatus, Integer serviceId, long timestamp) {
         int serviceStatusQuark = ssb.getQuarkRelativeAndAdd(getServiceQuark(ssb, serviceId), SERVICE_STATUS);
-        ssb.modifyAttribute(timestamp, serviceStatus, serviceStatusQuark);
+        ssb.modifyAttribute(timestamp, serviceStatus.toString(), serviceStatusQuark);
     }
 }
