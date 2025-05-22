e55f74b4409e60acbe2039ccdccc047b4ae17f4a
Bernd Hufmann <bernd.hufmann@ericsson.com>
2025-05-22 14:57:16 -0400

    swagger: Fix TraceQueryParameters and ExperimentQueryParameters
    Fixes:
    https://github.com/eclipse-cdt-cloud/trace-server-protocol/issues/100
    
    Signed-off-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core/src/org/eclipse/tracecompass/incubator/internal/trace/server/jersey/rest/core/model/TraceQueryParameters.java
+++ trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core/src/org/eclipse/tracecompass/incubator/internal/trace/server/jersey/rest/core/model/TraceQueryParameters.java
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2021 Ericsson
+ * Copyright (c) 2021, 2025 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License 2.0 which
@@ -23,22 +23,34 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public interface TraceQueryParameters {
 
     /**
-     * @return The name.
+     * @return The parameters.
      */
-    @Schema(description = "The name of the trace in the server, to override the default name")
-    String getName();
-
-    /**
-     * @return The type ID.
-     */
-    @Schema(description = "The trace type's ID, to force the use of a parser / disambiguate the trace type")
-    String getTypeID();
-
-    /**
-     * @return The URI.
-     */
-    @JsonProperty("uri")
     @NonNull
-    @Schema(description = "URI of the trace", required = true)
-    String getUri();
+    @Schema(required = true)
+    TraceParameters getParameters();
+
+    /**
+     * Detailed trace parameters.
+     */
+    interface TraceParameters {
+        /**
+         * @return The name.
+         */
+        @Schema(description = "The name of the trace in the server, to override the default name")
+        String getName();
+
+        /**
+         * @return The type ID.
+         */
+        @Schema(description = "The trace type's ID, to force the use of a parser / disambiguate the trace type")
+        String getTypeID();
+
+        /**
+         * @return The URI.
+         */
+        @JsonProperty("uri")
+        @NonNull
+        @Schema(description = "URI of the trace", required = true)
+        String getUri();
+    }
 }
