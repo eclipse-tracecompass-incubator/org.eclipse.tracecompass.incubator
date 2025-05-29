dad714c24d428f241c1957690a7859ed1ff2f527
Bernd Hufmann <bernd.hufmann@ericsson.com>
2025-05-29 11:05:05 -0400

    server: Only serialize supported value types in OutputElementStyle
    This will make sure that only objects that comply with the TSP
    specification are serialized.
    
    Added unit tests to verify this.
    
    https://github.com/eclipse-cdt-cloud/trace-server-protocol/issues/102
    
    Signed-off-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

---
+++ trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests/src/org/eclipse/tracecompass/incubator/trace/server/jersey/rest/core/tests/webapp/AbstractSerializerTest.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Ericsson and others
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License 2.0 which
+ * accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests.webapp;
+
+import org.junit.Before;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Abstract test class for testing serializers
+ *
+ * @author Bernd Hufmann
+ */
+public class AbstractSerializerTest {
+
+    /**
+     * A object mapper to be used for serialization / deserialization. A new
+     * instance is created per test case.
+     */
+    protected ObjectMapper fMapper;
+
+    /**
+     * Test setup
+     */
+    @Before
+    public void setup() {
+        fMapper = new ObjectMapper();
+    }
+}
