1434effaefe1ce85910faa4321c3a13673a8c474
Bernd Hufmann <bernd.hufmann@ericsson.com>
2025-10-07 11:58:11 -0400

    server.tests: Use TSP client in IdentifierServiceTest
    Signed-off-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests/src/org/eclipse/tracecompass/incubator/trace/server/jersey/rest/core/tests/services/IdentifierServiceTest.java
+++ trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests/src/org/eclipse/tracecompass/incubator/trace/server/jersey/rest/core/tests/services/IdentifierServiceTest.java
@@ -14,14 +14,12 @@ package org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests.s
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 import org.eclipse.tracecompass.incubator.internal.trace.server.jersey.rest.core.services.EndpointConstants;
 import org.eclipse.tracecompass.incubator.internal.trace.server.jersey.rest.core.services.IdentifierService;
-import org.eclipse.tracecompass.incubator.internal.trace.server.jersey.rest.core.services.ServerInfoResponseImpl;
-import org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests.utils.RestServerTest;
+import org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests.utils.NewRestServerTest;
+import org.eclipse.tracecompass.incubator.tsp.client.core.ApiException;
+import org.eclipse.tracecompass.incubator.tsp.client.core.api.IdentifierApi;
+import org.eclipse.tracecompass.incubator.tsp.client.core.model.ServerInfoResponse;
 import org.junit.Test;
 
 /**
@@ -29,31 +27,29 @@ import org.junit.Test;
  *
  * @author Vlad Arama
  */
-@SuppressWarnings("null")
-public class IdentifierServiceTest extends RestServerTest {
+public class IdentifierServiceTest extends NewRestServerTest {
+
+    private static final IdentifierApi sfIdentifierApi = new IdentifierApi(sfApiClient);
 
     /**
      * Test basic operations on the Identifier Service
+     *
+     * @throws ApiException
+     *             if such exception occurs
      */
     @Test
-    public void testIdentifier() {
-        WebTarget application = getApplicationEndpoint();
-        WebTarget identifierEndpoint = application.path("identifier");
+    public void testIdentifier() throws ApiException {
 
-        try (Response response = identifierEndpoint.request(MediaType.APPLICATION_JSON)
-                .get()) {
-            ServerInfoResponseImpl responseValues = response.readEntity(ServerInfoResponseImpl.class);
-
-            assertNotNull("Server version should not be null", responseValues.getVersion());
-            assertNotNull("OS should not be null", responseValues.getOs());
-            assertNotNull("OS Architecture should not be null", responseValues.getOsArch());
-            assertNotNull("OS Version should not be null", responseValues.getOsVersion());
-            assertNotNull("CPU count should not be null", responseValues.getCpuCount());
-            assertNotNull("Max memory should not be null", responseValues.getMaxMemory());
-            assertNotNull("Product ID should not be null", responseValues.getProductId());
-            assertNotNull("The TSP version should not be null", responseValues.getProductId());
-            assertEquals(EndpointConstants.VERSION, responseValues.getTspVersion());
-        }
+        ServerInfoResponse responseValues = sfIdentifierApi.getSystemInfo();
+        assertNotNull(responseValues);
+        assertNotNull("Server version should not be null", responseValues.getVersion());
+        assertNotNull("OS should not be null", responseValues.getOs());
+        assertNotNull("OS Architecture should not be null", responseValues.getOsArch());
+        assertNotNull("OS Version should not be null", responseValues.getOsVersion());
+        assertNotNull("CPU count should not be null", responseValues.getCpuCount());
+        assertNotNull("Max memory should not be null", responseValues.getMaxMemory());
+        assertNotNull("Product ID should not be null", responseValues.getProductId());
+        assertNotNull("The TSP version should not be null", responseValues.getProductId());
+        assertEquals(EndpointConstants.VERSION, responseValues.getTspVersion());
     }
-
 }
