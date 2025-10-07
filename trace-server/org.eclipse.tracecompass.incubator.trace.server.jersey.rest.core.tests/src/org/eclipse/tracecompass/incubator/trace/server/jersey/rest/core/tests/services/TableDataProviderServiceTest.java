5b5c0fd701fd81d6b7f5f5dc02edbf5e06c08943
Bernd Hufmann <bernd.hufmann@ericsson.com>
2025-10-07 09:14:59 -0400

    server.tests: Use TSP client in TableDataProviderServiceTest
    Signed-off-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests/src/org/eclipse/tracecompass/incubator/trace/server/jersey/rest/core/tests/services/TableDataProviderServiceTest.java
+++ trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests/src/org/eclipse/tracecompass/incubator/trace/server/jersey/rest/core/tests/services/TableDataProviderServiceTest.java
@@ -14,28 +14,25 @@ package org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests.s
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.ws.rs.ProcessingException;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
-
-import org.eclipse.tracecompass.incubator.internal.trace.server.jersey.rest.core.model.views.QueryParameters;
 import org.eclipse.tracecompass.incubator.internal.trace.server.jersey.rest.core.services.DataProviderService;
-import org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests.stubs.ColumnHeaderEntryStub;
-import org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests.stubs.ExperimentModelStub;
-import org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests.stubs.LineModelStub;
-import org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests.stubs.TableColumnsOutputResponseStub;
-import org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests.stubs.TableLinesOutputResponseStub;
-import org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests.stubs.TableModelStub;
-import org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests.utils.RestServerTest;
+import org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core.tests.utils.NewRestServerTest;
+import org.eclipse.tracecompass.incubator.tsp.client.core.ApiException;
+import org.eclipse.tracecompass.incubator.tsp.client.core.api.VirtualTablesApi;
+import org.eclipse.tracecompass.incubator.tsp.client.core.model.Experiment;
+import org.eclipse.tracecompass.incubator.tsp.client.core.model.LinesParameters;
+import org.eclipse.tracecompass.incubator.tsp.client.core.model.LinesQueryParameters;
+import org.eclipse.tracecompass.incubator.tsp.client.core.model.OptionalQueryParameters;
+import org.eclipse.tracecompass.incubator.tsp.client.core.model.TableColumnHeader;
+import org.eclipse.tracecompass.incubator.tsp.client.core.model.TableColumnHeadersResponse;
+import org.eclipse.tracecompass.incubator.tsp.client.core.model.VirtualTableLine;
+import org.eclipse.tracecompass.incubator.tsp.client.core.model.VirtualTableModel;
+import org.eclipse.tracecompass.incubator.tsp.client.core.model.VirtualTableResponse;
 import org.junit.Test;
 
 /**
@@ -43,18 +40,16 @@ import org.junit.Test;
  *
  * @author Bernd Hufmann
  */
-@SuppressWarnings("null")
-public class TableDataProviderServiceTest extends RestServerTest {
+public class TableDataProviderServiceTest extends NewRestServerTest {
     private static final String DATA_PROVIDER_RESPONSE_FAILED_MSG = "There should be a positive response for the data provider";
     private static final String MODEL_NULL_MSG = "The model is null, maybe the analysis did not run long enough?";
     private static final int MAX_ITER = 40;
     private static final String EVENTS_TABLE_DATAPROVIDER_ID = "org.eclipse.tracecompass.internal.provisional.tmf.core.model.events.TmfEventTableDataProvider";
 
-    private static final String REQUESTED_COLUMN_IDS_KEY = "requested_table_column_ids";
-    private static final String REQUESTED_TABLE_INDEX_KEY = "requested_table_index";
-    private static final String REQUESTED_TABLE_COUNT_KEY = "requested_table_count";
     private static final long TABLE_INDEX = 0L;
-    private static final long TABLE_COUNT = 100L;
+    private static final int TABLE_COUNT = 100;
+
+    private static final VirtualTablesApi sfTableApi = new VirtualTablesApi(sfApiClient);
 
     /**
      * Ensure that a table data provider exists and returns correct data. It
@@ -63,72 +58,62 @@ public class TableDataProviderServiceTest extends RestServerTest {
      *
      * @throws InterruptedException
      *             Exception thrown while waiting to execute again
+     * @throws ApiException if such exception occurs
      */
     @Test
-    public void testTableDataProvider() throws InterruptedException {
+    public void testTableDataProvider() throws InterruptedException, ApiException {
         long start = 1412670961211260539L;
         long end = 1412670967217750839L;
-        try {
-            ExperimentModelStub exp = assertPostExperiment(sfArm64KernelNotIntitialzedStub.getName(), sfArm64KernelNotIntitialzedStub);
+        Experiment exp = assertPostExperiment(sfArm64KernelNotIntitialzedStub.getName(), sfArm64KernelNotIntitialzedStub);
 
-            // Test getting the tree endpoint for an XY chart
-            WebTarget tableColumns = getTableColumnsEndpoint(exp.getUUID().toString(), EVENTS_TABLE_DATAPROVIDER_ID);
+        // Test getting the columns
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put(REQUESTED_TIMES_KEY, List.of(start, end));
+        OptionalQueryParameters queryParameters = new OptionalQueryParameters().parameters(parameters);
+        TableColumnHeadersResponse responseModel = sfTableApi.getColumns(exp.getUUID(), EVENTS_TABLE_DATAPROVIDER_ID, queryParameters);
+        assertNotNull(responseModel);
+        assertFalse(DATA_PROVIDER_RESPONSE_FAILED_MSG, TableColumnHeadersResponse.StatusEnum.FAILED.equals(responseModel.getStatus()));
 
-            Map<String, Object> parameters = new HashMap<>();
-            parameters.put(REQUESTED_TIMES_KEY, List.of(start, end));
-            TableColumnsOutputResponseStub responseModel;
-            try (Response tree = tableColumns.request().post(Entity.json(new QueryParameters(parameters, Collections.emptyList())))) {
-                assertEquals(DATA_PROVIDER_RESPONSE_FAILED_MSG, 200, tree.getStatus());
-                responseModel = tree.readEntity(TableColumnsOutputResponseStub.class);
-                assertNotNull(responseModel);
-            }
-            // Make sure the analysis ran enough and we have a model
-            int iteration = 0;
-            while (responseModel.isRunning() && responseModel.getModel() == null && iteration < MAX_ITER) {
-                Thread.sleep(100);
-                try (Response xyResponse = tableColumns.request().post(Entity.json(new QueryParameters(parameters, Collections.emptyList())))) {
-                    assertEquals(DATA_PROVIDER_RESPONSE_FAILED_MSG, 200, xyResponse.getStatus());
-                    responseModel = xyResponse.readEntity(TableColumnsOutputResponseStub.class);
-                    assertNotNull(responseModel);
-                    iteration++;
-                }
-            }
-
-            List<ColumnHeaderEntryStub> columns = responseModel.getModel();
-            assertNotNull(MODEL_NULL_MSG + responseModel, columns);
-            assertFalse(columns.isEmpty());
-
-            // Test getting the XY series endpoint
-            WebTarget tableLinesEnpoint = getTableLinesEndpoint(exp.getUUID().toString(), EVENTS_TABLE_DATAPROVIDER_ID);
-            List<Long> requestedColumnsIds = new ArrayList<>();
-            for (int i = 0; i <= columns.size() / 2; i++) {
-                requestedColumnsIds.add(columns.get(i).getId());
-            }
-            parameters.put(REQUESTED_COLUMN_IDS_KEY, requestedColumnsIds);
-            parameters.put(REQUESTED_TABLE_INDEX_KEY, TABLE_INDEX);
-            parameters.put(REQUESTED_TABLE_COUNT_KEY, TABLE_COUNT);
-            try (Response linesResponse = tableLinesEnpoint.request().post(Entity.json(new QueryParameters(parameters, Collections.emptyList())))) {
-                assertEquals(DATA_PROVIDER_RESPONSE_FAILED_MSG, 200, linesResponse.getStatus());
-                TableLinesOutputResponseStub lineModelResponse = linesResponse.readEntity(TableLinesOutputResponseStub.class);
-                assertNotNull(lineModelResponse);
-
-                TableModelStub tableModel = lineModelResponse.getModel();
-                assertNotNull("Table model", tableModel);
-                List<LineModelStub> lines = tableModel.getLines();
-                // FIXME This assert does not work with current implementation
-                // assertEquals("Sizes match", tableModel.getSize(), lines.size());
-                assertFalse(lines.isEmpty());
-                for (LineModelStub line : lines) {
-                    assertEquals("Number of returned cells", requestedColumnsIds.size(), line.getCells().size());
-                }
-            }
-
-        } catch (ProcessingException e) {
-            // The failure from this exception alone is not helpful. Use the
-            // suppressed exception's message be the failure message for more
-            // help debugging failed tests.
-            fail(e.getCause().getMessage());
+        // Make sure the analysis ran enough and we have a model
+        int iteration = 0;
+        while ((responseModel.getStatus().equals(TableColumnHeadersResponse.StatusEnum.RUNNING)) || (iteration < MAX_ITER)) {
+            Thread.sleep(100);
+            responseModel = sfTableApi.getColumns(exp.getUUID(), EVENTS_TABLE_DATAPROVIDER_ID, queryParameters);
+            assertNotNull(responseModel);
+            iteration++;
         }
+
+        List<TableColumnHeader> columns = responseModel.getModel();
+        assertNotNull(MODEL_NULL_MSG + responseModel, columns);
+        assertFalse(columns.isEmpty());
+
+        List<Long> requestedColumnsIds = new ArrayList<>();
+        for (int i = 0; i <= columns.size() / 2; i++) {
+            requestedColumnsIds.add(columns.get(i).getId());
+        }
+
+        // Test getting the lines
+        LinesParameters params = new LinesParameters()
+                .requestedTableColumnIds(requestedColumnsIds)
+                .requestedTableIndex(TABLE_INDEX)
+                .requestedTableCount(TABLE_COUNT)
+                .requestedTimes(null) // set to null to make sure the field is not serialized as empty array
+                .tableSearchExpressions(null); // set to null to make sure the field is not serialized as empty map
+
+        LinesQueryParameters linesQueryParameter = new LinesQueryParameters().parameters(params);
+        VirtualTableResponse lineModelResponse = sfTableApi.getLines(exp.getUUID(), EVENTS_TABLE_DATAPROVIDER_ID, linesQueryParameter);
+        assertNotNull(lineModelResponse);
+        VirtualTableModel tableModel =  lineModelResponse.getModel();
+        assertNotNull("Table model", tableModel);
+        List<VirtualTableLine> lines = tableModel.getLines();
+        // FIXME This assert does not work with current implementation
+        // assertEquals("Sizes match", tableModel.getSize(), lines.size());
+        assertFalse(lines.isEmpty());
+        for (VirtualTableLine line : lines) {
+            assertEquals("Number of returned cells", requestedColumnsIds.size(), line.getCells().size());
+        }
+
+        // TODO add search tests
     }
 
     /**
@@ -136,8 +121,8 @@ public class TableDataProviderServiceTest extends RestServerTest {
      */
     @Test
     public void testTableErrors() {
-        ExperimentModelStub exp = assertPostExperiment(sfArm64KernelNotIntitialzedStub.getName(), sfArm64KernelNotIntitialzedStub);
-        executePostErrorTests(exp, RestServerTest::getTableColumnsEndpoint, EVENTS_TABLE_DATAPROVIDER_ID, false);
-        executePostErrorTests(exp, RestServerTest::getTableLinesEndpoint, EVENTS_TABLE_DATAPROVIDER_ID, true);
+        Experiment exp = assertPostExperiment(sfArm64KernelNotIntitialzedStub.getName(), sfArm64KernelNotIntitialzedStub);
+        executePostErrorTests(exp.getUUID(), NewRestServerTest::getTableColumnsEndpoint, EVENTS_TABLE_DATAPROVIDER_ID, false);
+        executePostErrorTests(exp.getUUID(), NewRestServerTest::getTableLinesEndpoint, EVENTS_TABLE_DATAPROVIDER_ID, true);
     }
 }
